b1d3add58bec332bed6831a95895a08c733982d4
maciev <apcpc951995@gmail.com>
2020-04-25 15:42:59 -0400

    database password

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   'localhost');
-define('DATABASE_USERNAME', 'linkbtad_main');
-define('DATABASE_PASSWORD', 'ftf51678');
-define('DATABASE_NAME',     'linkbtad_main');
-define('SITE_URL',          'https://linkpage.cc/');
+define('DATABASE_SERVER',   'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	');
+define('DATABASE_USERNAME', 'o91u5jo8rd2r9h8h');
+define('DATABASE_PASSWORD', 'ziv804pta6f3ionv');
+define('DATABASE_NAME',     'rz7biis7t1n25j2j');
+define('DATABASE_PORT',          '3306');
