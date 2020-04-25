99547d371f13fbf8a65194c0f0c6164931b6e654
maciev <apcpc951995@gmail.com>
2020-04-25 15:50:19 -0400

    space delete

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	');
+define('DATABASE_SERVER',   'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 define('DATABASE_USERNAME', 'o91u5jo8rd2r9h8h');
 define('DATABASE_PASSWORD', 'ziv804pta6f3ionv');
 define('DATABASE_NAME',     'rz7biis7t1n25j2j');
